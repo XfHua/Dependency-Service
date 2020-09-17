ff3b67ba251b02a4fa40e45b85e76f2316c40315
XfHua <jasonHuaxf@163.com>
2020-09-17 10:33:03 +0800

    Add files via upload

---
+++ App384/App384/App384.Android/TextRecognition.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using App384.Droid;
+
+[assembly: Xamarin.Forms.Dependency(typeof(TextRecognition))]
+namespace App384.Droid
+{
+    [Preserve(AllMembers = true)]
+    [Activity(Label = "TextRecognition", Theme = "@style/Theme.AppCompat.Light.NoActionBar", MainLauncher = true)]
+    public class TextRecognition : AppCompatActivity, ITextRecognition
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+        }
+
+        public void LaunchActivityInAndroid()
+        {
+            Console.WriteLine("LaunchActivityInAndroid");
+        }
+
+    }
+}
---
+++ App384/App384/App384/FoodMenu.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App384
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FoodMenu : ContentPage
+    {
+        public FoodMenu()
+        {
+            InitializeComponent();
+        }
+
+        public void Food_CameraEntry(object sender, EventArgs e)
+        {
+            Xamarin.Forms.DependencyService.Register<ITextRecognition>();
+            DependencyService.Get<ITextRecognition>().LaunchActivityInAndroid();
+
+        }
+    }
+    public interface ITextRecognition
+    {
+        void LaunchActivityInAndroid();
+    }
+}
